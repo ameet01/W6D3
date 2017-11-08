d50e40a072ffd151ae2ce615ef12d3492f904c3d
Ameet Vadhia <ameet01@gmail.com>
2017-11-08 14:40:27 -0800

    implement follw toggle

--- AJAX Twitter/app/assets/javascripts/bundle.js
+++ AJAX Twitter/app/assets/javascripts/bundle.js
@@ -65,9 +65,72 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const FollowToggle = __webpack_require__(1);
+
+
+$(function() {
+  $('button.follow-toggle').each( (i, btn) => new FollowToggle(btn, {}) );
+});
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
+class FollowToggle {
+  
+  constructor(el, options) {
+    this.$el = $(el);
+    this.userId = this.$el.data('user-id') || options.userId;
+    this.followState = this.$el.data('initial-follow-state') || options.followState;
+    this.render();
+    this.$el.on('click', this.handleClick.bind(this) );
+  }
+  
+  render() {
+    if(this.followState === 'followed') {
+      this.$el.prop('disabled', false);
+      this.$el.html('Unfollow!');
+    } else {
+      this.$el.prop('disabled', false);
+      this.$el.html('Follow!');
+    }
+  }
+  
+  handleClick(e) {
+    var self = this;
+    
+    e.preventDefault();
+    
+    if(self.followState === 'followed') {
+      self.render();
+      $.ajax({
+        method: 'DELETE',
+        url: `/users/${self.userId}/follow`,
+        success: function(response) {
+          self.followState = 'unfollowed';
+          self.render();
+        }
+      });
+    }
+    if(self.followState === 'unfollowed') {
+      self.render();
+      $.ajax({
+        method: 'POST',
+        url: `/users/${self.userId}/follow`,
+        success: function(response) {
+          self.followState = 'followed';
+          self.render();
+        }
+      });
+    }
+  }
+  
+}
 
+
+module.exports = FollowToggle;
 
 /***/ })
 /******/ ]);
